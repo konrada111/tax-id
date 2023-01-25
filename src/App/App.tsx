2a3843ed423ca4fd31809f6439badc216362581b
konrada111 <konrada111@gmail.com>
2023-01-25 13:01:52 +0100

    Add home screen

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Home from "Home"
 import theme from "shared/constants/theme"
 import { GlobalStyle } from "shared/styles/GlobalStyle"
 import { ThemeProvider } from "styled-components"
@@ -7,6 +8,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Home />
     </ThemeProvider>
   )
 }
---
+++ src/Home/Home.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { useForm } from "react-hook-form"
+import Button from "shared/components/Button"
+import TextField from "shared/components/TextField"
+import { NIP_REGEX } from "shared/constants/regex"
+
+const Home = () => {
+  const { control } = useForm()
+
+  return (
+    <div>
+      <TextField
+        name="nip"
+        placeholder="NIP"
+        control={control}
+        rules={{
+          required: "Uzupełnij NIP",
+          pattern: {
+            value: new RegExp(NIP_REGEX),
+            message: "Nieprawidłowy format",
+          },
+        }}
+      />
+      <Button label="Szukaj" />
+    </div>
+  )
+}
+
+export default Home
