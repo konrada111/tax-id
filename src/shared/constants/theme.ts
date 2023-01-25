315464b574e5400ce85f844f5312ec8abe1a0920
konrada111 <konrada111@gmail.com>
2023-01-25 09:19:33 +0100

    Add theme for styled components

---
+++ src/shared/constants/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components"
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#1D62CB",
+    secondary: "#144EA4",
+    font: "#24282d",
+  },
+}
+
+export default theme
