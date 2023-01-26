952de00884a0f9b7511566bdc2e6e7ed33d5aed9
konrada111 <konrada111@gmail.com>
2023-01-26 21:57:20 +0100

    Update type for error handling

--- src/Home/redux/api.ts
+++ src/Home/redux/api.ts
@@ -7,8 +7,6 @@ export const getVat = async (vat_number: string) => {
     const { data } = await apiClient.get<TaxPayerType>("", {
       params: { vat_number },
     })
-
-    console.log("api data :", data)
     return data
   } catch (e) {
     if (axios.isAxiosError(e)) {
