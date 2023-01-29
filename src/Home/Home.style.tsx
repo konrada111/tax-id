00294d7b352b013b52a53814aa4874cfb3c2c4d8
konrada111 <konrada111@gmail.com>
2023-01-29 19:36:50 +0100

    Add grid areas

--- src/Home/Home.style.tsx
+++ src/Home/Home.style.tsx
@@ -1,30 +1,93 @@
-import { rem } from "shared/utils/style-utils"
+import ButtonComponent from "shared/components/Button"
+import { font, rem } from "shared/utils/style-utils"
 import styled from "styled-components"
 
 export const Wrapper = styled.div`
+  display: grid;
+  grid-template-areas:
+    ". form rightSide"
+    ". recent rightSide"
+    "leftSide box rightSide";
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 0.5fr 0.5fr 1fr 1.5fr;
+
+  @media (max-width: 992px) {
+    grid-template-areas:
+      "form"
+      "recent"
+      "box";
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.5fr 1fr 1fr;
+    padding: 5%;
+  }
+`
+
+export const Button = styled(ButtonComponent)`
+  margin-top: ${rem(4)};
+`
+
+export const LeftSide = styled.div`
+  grid-area: leftSide;
+  @media (max-width: 992px) {
+    display: none;
+  }
+`
+export const RightSide = styled.div`
+  grid-area: rightSide;
+  margin-top: ${rem(50)};
+
+  @media (max-width: 992px) {
+    display: none;
+  }
+`
+
+export const ReacentWrapper = styled.div`
+  grid-area: recent;
   display: flex;
   flex-direction: column;
-  flex: 1;
-  width: 100%;
+`
+
+export const FormWrapper = styled.div`
+  grid-area: form;
 `
 
 export const Form = styled.form`
   display: flex;
-  align-self: center;
-  flex: 1;
-  align-items: center;
   justify-content: center;
-  padding: ${rem(40)};
-  gap: ${rem(8)};
+  align-self: center;
+  margin: ${rem(40)} 0 ${rem(20)} 0;
+  gap: ${rem(16)};
+  flex-basis: 100%;
 `
+
+export const RecentSearchWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: ${rem(20)};
+  align-self: center;
+  margin-top: ${rem(20)};
+`
+
 export const Box = styled.div`
+  grid-area: box;
+  margin-top: ${rem(50)};
   display: flex;
   flex-direction: column;
   backgorund-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0px 0px 16px -7px rgba(15, 16, 16, 1);
   border-radius: ${rem(8)};
-  max-width: 700px;
+
   padding: ${rem(20)};
   gap: ${rem(20)};
   align-self: center;
+`
+export const Image = styled.img`
+  margin-left: ${rem(40)};
+`
+
+export const Info = styled.p`
+  text-align: center;
+  ${font("Lato", 18, 400, 26)};
+  color: ${({ theme }) => theme.colors.black};
 `
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,12 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
-import Button from "shared/components/Button"
 import TextField from "shared/components/TextField"
-import { NIP_REGEX } from "shared/constants/regex"
+import { NIP_REGEX } from "shared/constants/regex/nip"
 import { useDispatch, useSelector } from "react-redux"
 import { AppState } from "app/store"
-import { taxPayerActions } from "./redux/slice"
-import { Box, Form, Wrapper } from "./Home.style"
+import { taxPayerActions, TaxPayerType } from "./redux/slice"
+import man from "shared/assets/images/man.png"
+import girl from "shared/assets/images/girl.png"
+import {
+  Box,
+  Form,
+  Wrapper,
+  Button,
+  Info,
+  RecentSearchWrapper,
+  LeftSide,
+  RightSide,
+  FormWrapper,
+  ReacentWrapper,
+  Image,
+} from "./Home.style"
 import Row from "shared/components/Row"
 import {
   faBuildingColumns,
@@ -14,6 +27,9 @@ import {
   faAddressCard,
   faFile,
 } from "@fortawesome/free-solid-svg-icons"
+import { ErrorMessage } from "shared/components/TextField/TextField.style"
+import { useLocalStorage } from "shared/hooks/useLocalStorage"
+import ReacentSearch from "shared/components/ReacentSearch/ReacentSearch"
 
 export interface IFormInput {
   vat_number: string
@@ -25,28 +41,57 @@ const Home = () => {
   const dispatch = useDispatch()
   const taxPayer = useSelector((state: AppState) => state.taxpayer)
 
+  const localData = useLocalStorage<TaxPayerType[]>("taxPayers", [])
+
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     dispatch(taxPayerActions.fetchTaxPayer(data.vat_number))
   }
 
+  useEffect(() => {
+    console.log("local", localData)
+  }, [localData])
+
   return (
     <Wrapper>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          name="vat_number"
-          placeholder="NIP"
-          control={control}
-          rules={{
-            required: "Uzupełnij NIP ",
-            pattern: {
-              value: NIP_REGEX,
-              message: "Nieprawidłowy format",
-            },
-          }}
-        />
-        <Button type="submit" label="Szukaj" />
-      </Form>
-      {taxPayer.data && (
+      <LeftSide>
+        <Image src={girl} alt="girl" />
+      </LeftSide>
+
+      <FormWrapper>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            name="vat_number"
+            placeholder="NIP"
+            control={control}
+            rules={{
+              required: "Uzupełnij NIP ",
+              pattern: {
+                value: NIP_REGEX,
+                message: "Nieprawidłowy format",
+              },
+            }}
+          />
+          <Button type="submit" label="Szukaj" />
+        </Form>
+      </FormWrapper>
+
+      <RightSide>
+        <Image src={man} alt="man" />
+      </RightSide>
+      <ReacentWrapper>
+        <h2>Recent Search</h2>
+        <RecentSearchWrapper>
+          {localData.map(({ query, company_name }, idx) => (
+            <ReacentSearch
+              key={idx}
+              companyName={company_name}
+              vatNumber={query}
+            />
+          ))}
+        </RecentSearchWrapper>
+      </ReacentWrapper>
+
+      {taxPayer.data?.valid && (
         <Box>
           <Row
             icon={faBuildingColumns}
@@ -70,6 +115,13 @@ const Home = () => {
           />
         </Box>
       )}
+      {taxPayer.data?.valid === false && (
+        <Info>
+          Your vat number is invalid, cannot find it in our database (sample vat
+          number LU26375245)
+        </Info>
+      )}
+      {taxPayer.error && <ErrorMessage>{taxPayer.error}</ErrorMessage>}
     </Wrapper>
   )
 }
