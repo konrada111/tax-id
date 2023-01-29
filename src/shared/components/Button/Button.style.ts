8b1a79ce81222036bb20c848364d8dd92eb0a276
konrada111 <konrada111@gmail.com>
2023-01-29 19:37:55 +0100

    Add button

--- src/shared/components/Button/Button.style.ts
+++ src/shared/components/Button/Button.style.ts
@@ -5,10 +5,12 @@ export type ButtonVariants = "primary" | "secondary" | "tertiary"
 
 export const Wrapper = styled.button`
   ${font("Lato", 14, 500, 26)}
-  padding: ${rem(6)} ${rem(16)};
+  padding: 0 ${rem(16)};
+  height: ${rem(40)};
   border: none;
   border-radius: ${rem(8)};
   white-space: nowrap;
   background-color: ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.white};
+  width: fit-content;
 `
